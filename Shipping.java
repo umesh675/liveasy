f43e034d1b43dfc99784a97625f6ce071ecd7731
umesh675 <66577792+umesh675@users.noreply.github.com>
2022-02-11 21:51:14 +0530

    Add files via upload
    Class with shipping details

---
+++ Shipping.java
@@ -0,0 +1,98 @@
+package com.liveasy.liveasy;
+
+import javax.persistence.*;
+
+//class for shipping details
+@Entity
+public class Shipping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int loadId ;
+
+    private String loadingPoint ;
+    private String unloadingPoint;
+    private String productType;
+    private String truckType;
+    private String noOfTrucks;
+    private double weight;
+    private String comment ;
+
+    @Column(nullable = false)
+    private String shipperId;
+
+    private String date ;
+
+    public String getLoadingPoint() {
+        return loadingPoint;
+    }
+
+    public void setLoadingPoint(String loadingPoint) {
+        this.loadingPoint = loadingPoint;
+    }
+
+    public String getUnloadingPoint() {
+        return unloadingPoint;
+    }
+
+    public void setUnloadingPoint(String unloadingPoint) {
+        this.unloadingPoint = unloadingPoint;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getTruckType() {
+        return truckType;
+    }
+
+    public void setTruckType(String truckType) {
+        this.truckType = truckType;
+    }
+
+    public String getNoOfTrucks() {
+        return noOfTrucks;
+    }
+
+    public void setNoOfTrucks(String noOfTrucks) {
+        this.noOfTrucks = noOfTrucks;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getShipperId() {
+        return shipperId;
+    }
+
+    public void setShipperId(String shipperId) {
+        this.shipperId = shipperId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
